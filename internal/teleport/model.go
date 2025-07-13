b1f4e845ab5a1158ec5186cdb3b701f9affbb983
steamedEggMaster <iron.ch5014@gmail.com>
2025-07-13 22:57:07 +0900

    refactor: change GetAccessRequestableRoles method
    - service.go
    1. change Method name to GetUserAccessInfo
    
    - model.go
    1. define UserAccessInfo struct

--- internal/teleport/model.go
+++ internal/teleport/model.go
@@ -3,3 +3,8 @@ package teleport
 type User struct {
 	Username string `json:"username"`
 }
+
+type UserAccessInfo struct {
+	Roles         []string `json:"roles"`
+	RequireReason bool     `json:"require_reason"`
+}
