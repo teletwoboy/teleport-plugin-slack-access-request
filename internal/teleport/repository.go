2014da7c1561678d639b97de2cee927059c2096e
steamedEggMaster <iron.ch5014@gmail.com>
2025-07-16 01:55:17 +0900

    feat: create TeleportUser in Database
    - teleport/
    -- model.go
    1. add entity mapping fields required for service logic
    
    -- repository.go
    1. write annotation for helping coworkers to understand Database Architecture
    2. define PostgresRepository struct that owns *database.DB
    3. add NewRepository function for creating teleport.PostgresRepository instance
    4. implement teleport.Repository's CreateUser method
    
    -- service.go
    1. define Repository Interface for reducing database implementation
    2. define CreateUser method in Repository interface
    3. add NewService function for creating teleport.Service instance
    4. add CreateUser method

---
+++ internal/teleport/repository.go
@@ -0,0 +1,42 @@
+package teleport
+
+import (
+	"context"
+	"fmt"
+	"teleport-plugin-slack-access-request/internal/database"
+	"teleport-plugin-slack-access-request/internal/database/sqlc"
+)
+
+type PostgresRepository struct {
+	db *database.DB
+}
+
+func NewRepository(db *database.DB) *PostgresRepository {
+	return &PostgresRepository{db: db}
+}
+
+func (r *PostgresRepository) CreateUser(ctx context.Context, user User) (*User, error) {
+	baseEntity := database.PrePersist()
+
+	createTeleportUserParams := sqlc.CreateTeleportUserParams{
+		Username:   user.Username,
+		UseYn:      baseEntity.UseYn,
+		CreateCode: baseEntity.CreateCode,
+		CreateDate: baseEntity.CreateDate,
+		Version:    baseEntity.Version,
+	}
+
+	createdTeleportUser, err := r.db.Queries.CreateTeleportUser(ctx, createTeleportUserParams)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create teleport user in DB: %w", err)
+	}
+
+	return &User{
+		TeleportUserID: createdTeleportUser.TeleportUserID,
+		Username:       createdTeleportUser.Username,
+		UseYn:          createdTeleportUser.UseYn,
+		CreateCode:     createdTeleportUser.CreateCode,
+		CreateDate:     createdTeleportUser.CreateDate,
+		Version:        createdTeleportUser.Version,
+	}, nil
+}
