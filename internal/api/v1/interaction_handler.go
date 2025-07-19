8d8f156441830f6d4230a4edbccb9ba685e211e7
steamedEggMaster <iron.ch5014@gmail.com>
2025-07-20 01:48:43 +0900

    feat: implement bring up review modal
    - message/builder.go
    1. 리뷰어가 리뷰 모달 요청을 보냈을 때 검증에서 발생하는 메시지 2가지를 추가하였습니다.
       1. accessRequestNotFoundBuilder : Access Request가 삭제되어(use_yn 이 false로 변경되어) 해당 요청이 없는 경우 메시지를 보내기 위한 구조체
                                         - 예시로 CLI 로 누군가 Review를 했을때 저희가 개발할 이벤트 기반 Access Request 삭제가 있습니다.
       2. accessRequestAlreadyApprovedBuilder : 이미 같은 채널의 다른 리뷰어가 리뷰한 경우 메시지를 보내기 위함입니다.
    
    - modal/payload.go
    1. AccessRequestBlockActionsPayload : 리뷰어가 리뷰 모달 요청을 보냈을 때 받은 json string 값을 매핑하기 위한 구조체입니다.
    2. 나머지는 내부에서 사용되는 구조체입니다.
    
    - modal/accessrequest_review.go
    1. accessRequestReviewBuilder : Review 모달을 생성하기 위한 구조체 입니다.
    
    - api/v1/interaction_handler.go
    1. HandleBlockActions : 리뷰어가 모달 요청 버튼 클릭에 대한 요청 핸들러입니다.
                            - 핸들러들 내부 로직은 이후에 리팩토링할 예정입니다.
                            - 일단 빠르게 구현 먼저 하겠습니다.

--- internal/api/v1/interaction_handler.go
+++ internal/api/v1/interaction_handler.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"teleport-plugin-slack-access-request/internal/slack"
@@ -48,6 +49,7 @@ func (i *InteractionHandler) Handle(w http.ResponseWriter, r *http.Request) {
 	switch slackapi.InteractionType(callbackType.Type) {
 	case slackapi.InteractionTypeBlockActions:
 		// 메시지에서 버튼 클릭 처리
+		i.HandleBlockActions(payloadStr, w)
 	case slackapi.InteractionTypeViewSubmission:
 		// 모달에서 submit 처리
 		// callback_id 로 분기 처리 가능
@@ -198,6 +200,149 @@ func (i *InteractionHandler) HandleViewSubmission(payloadStr string, w http.Resp
 		slog.Error("failed to post access request message for reviewers to slack", "channelID", reviewersChannelID, "err", err)
 		http.Error(w, "failed to post access request message for reviewers to slack", http.StatusInternalServerError)
 		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (i *InteractionHandler) HandleBlockActions(payloadStr string, w http.ResponseWriter) {
+	ctx := context.Background()
+
+	// 값 준비
+	var callback modal.AccessRequestBlockActionsPayload
+	if err := json.Unmarshal([]byte(payloadStr), &callback); err != nil {
+		http.Error(w, "invalid payload format", http.StatusBadRequest)
+		return
+	}
+
+	b, err := json.MarshalIndent(callback, "", "  ")
+	if err != nil {
+		fmt.Println("pretty print error:", err)
+		return
+	}
+	fmt.Println(string(b))
+
+	reviewersChannelID := callback.Channel.ID
+	reviewerID := callback.User.ID
+	reviewerName := callback.User.Name
+	accessRequestName := callback.Actions[0].Value
+	triggerID := callback.TriggerID
+
+	// 1. 검증
+	//    1. 메시지 버튼을 누른 사람이 Reviewers 채널에 있는 사람이 맞는지 확인
+	exists, err := i.SlackSrv.ExistsUserInChannelByID(reviewerID, reviewersChannelID)
+	if err != nil {
+		slog.Error("failed to check if user exists in slack channel", "error", err)
+		errorMessageBuilder := message.NewErrorMessageBuilder(err)
+		_, _, err := i.SlackSrv.PostMessage(reviewersChannelID, errorMessageBuilder)
+		if err != nil {
+			slog.Error("failed to post error message to slack", "channelID", reviewersChannelID, "err", err)
+			http.Error(w, "failed to post error message to slack", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if !exists {
+		slog.Error("User not found", "userID", reviewerID, "err", err)
+		userNotFoundMessageBuilder := message.NewUserNotFoundBuilder(reviewerName)
+		_, _, err := i.SlackSrv.PostMessage(reviewersChannelID, userNotFoundMessageBuilder)
+		if err != nil {
+			slog.Error("failed to post user not found message to slack", "channelID", reviewersChannelID, "err", err)
+			http.Error(w, "failed to post user not found message to slack", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	//    2. 요청이 존재하는 지 확인
+	exists, err = i.TeleportSrv.ExistsAccessRequestByName(ctx, accessRequestName)
+	if err != nil {
+		slog.Error("failed to check if access request exists in teleport service", "error", err)
+		errorMessageBuilder := message.NewErrorMessageBuilder(err)
+		_, _, err := i.SlackSrv.PostMessage(reviewersChannelID, errorMessageBuilder)
+		if err != nil {
+			slog.Error("failed to post error message to slack", "channelID", reviewersChannelID, "err", err)
+			http.Error(w, "failed to post error message to slack", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if !exists {
+		slog.Error("AccessRequest not found", "userID", reviewerID, "err", err)
+		accessRequestNotFoundBuilder := message.NewAccessRequestNotFoundBuilder(accessRequestName)
+		_, _, err := i.SlackSrv.PostMessage(reviewersChannelID, accessRequestNotFoundBuilder)
+		if err != nil {
+			slog.Error("failed to post error message to slack", "channelID", reviewersChannelID, "err", err)
+			http.Error(w, "failed to post error message to slack", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	//    3. 요청이 이미 승인되었는지 확인
+	accessRequest, err := i.TeleportSrv.GetAccessRequestByName(ctx, accessRequestName)
+	if err != nil {
+		slog.Error("failed to get access request status", "error", err)
+		errorMessageBuilder := message.NewErrorMessageBuilder(err)
+		_, _, err := i.SlackSrv.PostMessage(reviewersChannelID, errorMessageBuilder)
+		if err != nil {
+			slog.Error("failed to post error message to slack", "channelID", reviewersChannelID, "err", err)
+			http.Error(w, "failed to post error message to slack", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if accessRequest.Status == "APPROVED" {
+		alreadyApprovedBuilder := message.NewAccessRequestAlreadyApprovedBuilder(accessRequestName)
+		_, _, err := i.SlackSrv.PostMessage(reviewersChannelID, alreadyApprovedBuilder)
+		if err != nil {
+			slog.Error("failed to post error message to slack", "channelID", reviewersChannelID, "err", err)
+			http.Error(w, "failed to post error message to slack", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	slackUser, err := i.SlackSrv.GetUserBySlackUserID(ctx, accessRequest.RequesterUserID)
+	if err != nil {
+		slog.Error("failed to get user from slack", "error", err)
+		errorMessageBuilder := message.NewErrorMessageBuilder(err)
+		_, _, err := i.SlackSrv.PostMessage(reviewersChannelID, errorMessageBuilder)
+		if err != nil {
+			slog.Error("failed to post error message to slack", "channelID", reviewersChannelID, "err", err)
+			http.Error(w, "failed to post error message to slack", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// 2. 리뷰 모달 생성하기
+	//    다시 사용자가 요청한 내용 보여주기
+	//    Allow / Deny 리스트
+	//    Reason 칸
+	accessRequestReviewBuilder := modal.NewAccessRequestReviewBuilder(accessRequest, slackUser)
+
+	// 3. 모달 보내기
+	err = i.SlackSrv.OpenModal(triggerID, accessRequestReviewBuilder)
+	if err != nil {
+		slog.Error("failed to open modal", "err", err)
+		errorMessageBuilder := message.NewErrorMessageBuilder(err)
+		_, _, err := i.SlackSrv.PostMessage(reviewersChannelID, errorMessageBuilder)
+		if err != nil {
+			slog.Error("failed to post error message to slack", "channelID", reviewersChannelID, "err", err)
+			http.Error(w, "failed to post error message to slack", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	w.WriteHeader(http.StatusOK)
