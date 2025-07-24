d6869e9bef37a4952d671f0f901fe5b23fd8af7b
steamedEggMaster <iron.ch5014@gmail.com>
2025-07-24 15:39:41 +0900

    refactor: change receiver name from a to o

--- internal/api/v1/open_access_request_modal_handler.go
+++ internal/api/v1/open_access_request_modal_handler.go
@@ -25,7 +25,7 @@ func NewOpenAccessRequestModalHandler(s slack.Service, t teleport.Service, u use
 	}
 }
 
-func (a *OpenAccessRequestModalHandler) Handle(w http.ResponseWriter, r *http.Request) {
+func (o *OpenAccessRequestModalHandler) Handle(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	// 1. 페이로드 추출
@@ -36,39 +36,40 @@ func (a *OpenAccessRequestModalHandler) Handle(w http.ResponseWriter, r *http.Re
 	}
 
 	// 2. 검증
+	//    1. 데이터베이스에 해당 유저가 존재하는가?
 	slackVerifier := verifier.NewSlack(a.SlackSrv)
 	//    1. 데이터베이스에 해당 유저가 존재하는가?
 	if err := slackVerifier.VerifyUserExistsByID(ctx, payload.UserID, payload.UserName); err != nil {
-		res.ErrorMessageToSlack(a.SlackSrv, payload.ChannelID, err, w)
+		res.ErrorMessageToSlack(o.SlackSrv, payload.ChannelID, err, w)
 		return
 	}
 
 	// 3. Access Request Modal을 만들기 위한 데이터를 수집한다.
 	//    1. slack user
-	slackUser, err := a.SlackSrv.GetUserByID(ctx, payload.UserID)
+	slackUser, err := o.SlackSrv.GetUserByID(ctx, payload.UserID)
 	if err != nil {
-		res.ErrorMessageToSlack(a.SlackSrv, payload.ChannelID, err, w)
+		res.ErrorMessageToSlack(o.SlackSrv, payload.ChannelID, err, w)
 		return
 	}
 
 	//    2. teleport user
-	teleportUser, err := a.TeleportSrv.GetUserByUsername(ctx, slackUser.Email)
+	teleportUser, err := o.TeleportSrv.GetUserByUsername(ctx, slackUser.Email)
 	if err != nil {
-		res.ErrorMessageToSlack(a.SlackSrv, payload.ChannelID, err, w)
+		res.ErrorMessageToSlack(o.SlackSrv, payload.ChannelID, err, w)
 		return
 	}
 
 	//    1. AccessInfo
-	accessInfo, err := a.TeleportSrv.FetchUserAccessInfo(ctx, teleportUser)
+	accessInfo, err := o.TeleportSrv.FetchUserAccessInfo(ctx, teleportUser)
 	if err != nil {
-		res.ErrorMessageToSlack(a.SlackSrv, payload.ChannelID, err, w)
+		res.ErrorMessageToSlack(o.SlackSrv, payload.ChannelID, err, w)
 		return
 	}
 
 	//	  2. reviewersChannels
-	channels, err := a.SlackSrv.FetchReviewersChannels()
+	channels, err := o.SlackSrv.FetchReviewersChannels()
 	if err != nil {
-		res.ErrorMessageToSlack(a.SlackSrv, payload.ChannelID, err, w)
+		res.ErrorMessageToSlack(o.SlackSrv, payload.ChannelID, err, w)
 		return
 	}
 
@@ -76,8 +77,8 @@ func (a *OpenAccessRequestModalHandler) Handle(w http.ResponseWriter, r *http.Re
 	builder := modal.NewAccessRequestBuilder(accessInfo, channels, payload, slackUser)
 
 	// 5. 모달을 보낸다
-	if err := a.SlackSrv.OpenModal(payload.TriggerID, builder); err != nil {
-		res.ErrorMessageToSlack(a.SlackSrv, payload.ChannelID, err, w)
+	if err := o.SlackSrv.OpenModal(payload.TriggerID, builder); err != nil {
+		res.ErrorMessageToSlack(o.SlackSrv, payload.ChannelID, err, w)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
