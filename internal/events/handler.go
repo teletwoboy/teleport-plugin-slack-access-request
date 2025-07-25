f3216fc1caa1c3dd458d3e865cea853aedbb3a9e
nogie <layer7@kakao.com>
2025-07-25 12:22:26 +0900

    feat: add user to teleport_user table on MFA device addition
    service.go 에서는 모든 event 수신, handler.go 에서는 모든 event 핸들링을 위한 구조로 변경하였습니다.
    
    handler.go 에서 이후 모든 이벤트에 대한 핸들링을 하기 위해 각 서비스의 의존성을 주입받았습니다.
    
    DB에 존재하지 않는 경우에만 계정을 생성하는 로직을 작성하였습니다.

--- internal/events/handler.go
+++ internal/events/handler.go
@@ -1,18 +1,65 @@
 package events
 
 import (
+	"context"
+	"log/slog"
+	"teleport-plugin-slack-access-request/internal/database"
+
+	"teleport-plugin-slack-access-request/internal/slack"
+	"teleport-plugin-slack-access-request/internal/teleport"
+	teleportmodel "teleport-plugin-slack-access-request/internal/teleport/models"
+	"teleport-plugin-slack-access-request/internal/user"
+	"teleport-plugin-slack-access-request/internal/util/verifier"
 	"time"
 
 	"github.com/gravitational/teleport/api/types/events"
 )
 
-func HandleEvent(event events.AuditEvent) time.Time {
-	switch event.(type) {
+type EventHandler struct {
+	SlackSrv    slack.Service
+	TeleportSrv teleport.Service
+	UserSrv     user.Service
+}
+
+func NewEventHandler(s slack.Service, t teleport.Service, u user.Service) *EventHandler {
+	return &EventHandler{
+		SlackSrv:    s,
+		TeleportSrv: t,
+		UserSrv:     u,
+	}
+}
+
+func (h *EventHandler) TeleportEventHandle(ctx context.Context, event events.AuditEvent) time.Time {
+	teleportVerifier := verifier.NewTeleport(h.TeleportSrv)
+	switch e := event.(type) {
 	case *events.MFADeviceAdd:
+		dupCheck, err := teleportVerifier.VerifyUserExistsByID(ctx, e.User)
+		if err != nil {
+			slog.Error("failed to verify existing user", "err", err)
+		}
+
+		if dupCheck {
+			slog.Error("already exist user", "username", e.User)
+			return time.Time{}
+		}
+		h.createTeleportUser(ctx, e.User)
 		return event.GetTime()
 	case *events.UserDelete:
 		return event.GetTime()
 	default:
 		return event.GetTime()
+	}
+}
+
+func (h *EventHandler) createTeleportUser(ctx context.Context, email string) {
+	info := teleportmodel.User{
+		Username:   email,
+		UseYn:      true,
+		CreateCode: database.CreateCode,
+		CreateDate: time.Now().UTC(),
+	}
+	_, err := h.TeleportSrv.CreateUser(ctx, info)
+	if err != nil {
+		slog.Error("failed", "err", err)
 	}
 }
