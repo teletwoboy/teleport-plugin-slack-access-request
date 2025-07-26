d43194431a6efe3580ae6862b848bcaedbdc1b1f
steamedEggMaster <iron.ch5014@gmail.com>
2025-07-26 20:23:15 +0900

    refactor: modify event handler logic
    1. create.go 파일 삭제
       : 현재 해당 파일의 목적은 리터럴 기반의 구조체 생성이 코드가 지저분하기에, 팀원분이 분리하신거 같다.
         그래서 models 에 New 메서드를 추가하였고, Service 레벨에서 Create 도 지원하기에 삭제하였다.
    
    2. service.go 파일 삭제 후 event.go 파일 생성 및
       기존의 Service, API interface, service struct 구조를 사용하던 방식에서
       Service와 API 인터페이스를 지우고, Event struct에 필요한 필드를 받아서 사용하는 방식으로 설정
       - 이유
       : Event를 위해 Service, API interface를 만드는 것은 너무 과도한 분리임
         왜냐면,
         1. 외부에서 부를 함수 = EventWatcher 1개
         2. 내부에서 Event를 위해 클라이언트에서 실행할 함수 = NewWatcher 1개
         정작 Event 내부에서 많이 실행되는 것은 Handler 구조체의 함수들이다.
    
         이렇게 하기 보다는
         이미 Teleport Service는 Teleport Client를 가지고 있고,
         이 Service를 받아서 사용하는 것이 효율적이다.
         그리고 Handler 구조체들을 받아서 이벤트 종류에 따라 실행한다.
    
    2. event.go 이름은 events/ 패키지와 이름이 겹치기에 router.go 나 원래 이름대로 service.go 로 할지 팀원분과 토의해야한다.
    
    3. create_user_handler.go
       1. 기존에 service.go 에서 전부 실행되던 로직을 하나의 Handler로 나누어
          주석으로 단계를 정리하며 작성했습니다.
       2. 트랜잭션 처리가 필요한 중요한 작업이기에 적용하였습니다.

---
+++ internal/events/v1/create_user_handler.go
@@ -0,0 +1,105 @@
+package v1
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log/slog"
+	"teleport-plugin-slack-access-request/internal/database"
+	teleportmodels "teleport-plugin-slack-access-request/internal/teleport/models"
+	usermodels "teleport-plugin-slack-access-request/internal/user/models"
+	"teleport-plugin-slack-access-request/internal/util/container"
+	"teleport-plugin-slack-access-request/internal/util/verifier"
+
+	"github.com/gravitational/teleport/api/types"
+)
+
+type CreateUserHandler struct {
+	DB       *database.DB
+	Clients  *container.Clients
+	Services *container.Services
+}
+
+func NewCreateUserHandler(db *database.DB, c *container.Clients, s *container.Services) *CreateUserHandler {
+	return &CreateUserHandler{
+		DB:       db,
+		Clients:  c,
+		Services: s,
+	}
+}
+
+func (c *CreateUserHandler) Handle(ctx context.Context, resource *types.UserV2) {
+	// 1. 값 준비
+	username := resource.GetName()
+
+	// 2. 검증
+	slackVerifier := verifier.NewSlack(c.Services.Slack)
+	teleportVerifier := verifier.NewTeleport(c.Services.Teleport)
+	//    1. Teleport User가 데이터베이스에 존재하지 않는가?
+	check, err := teleportVerifier.VerifyUserNotExistsByUsername(ctx, username)
+	if err != nil {
+		slog.Error("failed to verify existing user", "err", err)
+		return
+	}
+	if !check {
+		slog.Info("user already exists in DB", "username", username)
+		return
+	}
+
+	//    2. Username을 갖는 Slack User가 Slack에 존재하는가?
+	slackUser, err := slackVerifier.VerifyUserExistsByUsernameFromClient(username)
+	if err != nil {
+		slog.Error("failed to verify existing user", "err", err)
+		return
+	}
+
+	// 3. 트랜잭션 시작하기
+	tx, err := c.DB.Conn.BeginTx(ctx, nil)
+	if err != nil {
+		slog.Error("failed to begin transaction", "err", err)
+		return
+	}
+	committed := false
+	defer func(tx *sql.Tx) {
+		if !committed {
+			if err := tx.Rollback(); err != nil && !errors.Is(err, sql.ErrTxDone) {
+				slog.Error("failed to rollback transaction", "err", err)
+			}
+		}
+	}(tx)
+
+	// 4. 트랜잭션이 적용된 Repositories, Services 만들기
+	qtx := c.DB.Queries.WithTx(tx)
+	txRepos := container.NewRepositories(qtx)
+	txServices := container.NewServices(c.Clients, txRepos)
+
+	// 5. Teleport User 저장하기
+	teleportUser := teleportmodels.NewUser(username)
+	createdTeleportUser, err := txServices.Teleport.CreateUser(ctx, teleportUser)
+	if err != nil {
+		slog.Error("failed to create teleport user", "err", err)
+		return
+	}
+
+	// 6. Slack User 저장하기
+	createdSlackUser, err := txServices.Slack.CreateUser(ctx, slackUser)
+	if err != nil {
+		slog.Error("failed to create slack user", "err", err)
+		return
+	}
+
+	// 7. User 저장하기
+	user := usermodels.NewUser(createdSlackUser, createdTeleportUser)
+	createdUser, err := txServices.User.CreateUser(ctx, user)
+	if err != nil {
+		slog.Error("failed to create user", "err", err)
+		return
+	}
+
+	// 8. 트랜잭션 종료하기
+	if err := tx.Commit(); err != nil {
+		slog.Error("failed to commit transaction", "err", err)
+		return
+	}
+	slog.Info("successfully created user", "username", createdUser.TeleportUser.Username)
+}
