d7e9824553a6abd80490cae8234122cb849ffd94
steamedEggMaster <iron.ch5014@gmail.com>
2025-08-06 21:57:25 +0900

    remove: healthz logging
    불필요한 로깅 삭제

--- internal/api/check/healthz.go
+++ internal/api/check/healthz.go
@@ -1,7 +1,6 @@
 package check
 
 import (
-	"log/slog"
 	"net/http"
 )
 
@@ -10,5 +9,4 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(http.StatusOK)
-	slog.Info("w", w, "OK")
 }
